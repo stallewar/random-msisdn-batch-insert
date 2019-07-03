c2b6224f2bee0828d8659aa1b2d2649ea9cc1a1a
stallewar <lebedev.nikolay@gmail.com>
2019-07-03 19:38:07 +0300

    info added

---
+++ README.md
@@ -0,0 +1,5 @@
+From test classes.
+Generate some millions Faker, "random" test data and insert it to database.
+Fast...;)
+
+run it from IDE or maven test lifecycle.
---
+++ src/main/java/org/tqa/utils/randommsisdbatchinsert/RandomMsisdbBatchInsertApplication.java
@@ -0,0 +1,13 @@
+package org.tqa.utils.randommsisdbatchinsert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomMsisdbBatchInsertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomMsisdbBatchInsertApplication.class, args);
+	}
+
+}
